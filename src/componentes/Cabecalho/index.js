25735073efc24d20e509994bb054ffae939d84c1
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-05-06 17:07:22 -0300

    adicionando componentes: Cabecalho, Banner, Rodape, Titulo, CabecalhoLink e Card

---
+++ src/componentes/Cabecalho/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from './Logo-cinetag-branco 1.png'
+import styles from './Cabecalho.module.css'
+import CabecalhoLink from 'componentes/CabecalhoLink'
+
+export default function Cabecalho() {
+    return (
+        <header className={styles.cabecalho}>
+            <Link to='./'>
+                <img src={Logo} alt='Logo do Cine Tag'/>
+            </Link>
+            <nav>
+                <CabecalhoLink url='./'>
+                    Home
+                </CabecalhoLink>
+                <CabecalhoLink url='./Favoritos'>
+                    Favoritos
+                </CabecalhoLink>
+            </nav>
+        </header>
+    )
+}
